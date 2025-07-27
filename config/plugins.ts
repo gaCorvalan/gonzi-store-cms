e16018853c0593b39657d59a099f257ed51e748e
gaCorvalan <gabrielcorvalan.dev@gmail.com>
2025-07-27 18:13:35 -0300

    feature: added categories, prodcuts, images, cloudinary integration etc

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,29 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {
+          folder: 'gonzi-store', // Carpeta en Cloudinary donde se guardarán los archivos
+          resource_type: 'auto', // Detecta automáticamente el tipo de archivo
+          use_filename: true,
+          unique_filename: false,
+          overwrite: false,
+        },
+        uploadStream: {
+          folder: 'gonzi-store',
+          resource_type: 'auto',
+          use_filename: true,
+          unique_filename: false,
+          overwrite: false,
+        },
+        delete: {},
+      },
+    },
+  },
+});
---
+++ src/api/product-set/services/product-set.ts
@@ -0,0 +1,7 @@
+/**
+ * product-set service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::product-set.product-set');
